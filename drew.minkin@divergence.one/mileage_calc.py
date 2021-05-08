e0f3dbda32f0042c2bd9a64198f9c8b93cc21269
archaeus@hekaplex.com <archaeus@hekaplex.com>
2021-05-08 15:38:10 -0500

    more samples

--- drew.minkin@divergence.one/mileage_calc.py
+++ drew.minkin@divergence.one/mileage_calc.py
@@ -9,8 +9,8 @@ miles_driven= float(input("Enter miles driven:\t\t"))
 gallons_used = float(input("Enter gallons of gas used:\t"))
 
 # calculate and round miles per gallon
-mpg = miles_driven / gallons_used
-mpg = round(mpg, 2)
+
+mpg = round(miles_driven / gallons_used, 2)
 
 # display the result
 print()
