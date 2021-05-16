286d150462744239e7729b3c7dd0c6508060960d
Tyarnes <Tyarnesbarton@yahoo.com>
2021-05-16 14:33:25 -0400

    Random Roll added

--- Ty Barton/lab1.py
+++ Ty Barton/lab1.py
@@ -1,4 +1,4 @@
-print('Enter your name:')
+print('What is your name?')
 x = input('Enter your name:')
 print('Hello,' + x)
 
@@ -8,4 +8,21 @@ print(x)
 print('Add 10 to your age')
 print("The sum is:",int(x) + 10)
 
-print(Roll the Dice)
+print("Remember this number later, it is your Lucky Number.")
+
+print("Welcome to the world created by you.")
+print("You Have a Six Sided Die, Each Roll Will Earn You Gear")
+
+def read_items():
+    items = []
+    with open("Items.txt") as file:
+        for line in file:
+            line = line.replace("\n", "")
+            items.append(line)
+    return items
+    
+import random
+
+mylist = read_items()
+
+print(random.choice(mylist))
